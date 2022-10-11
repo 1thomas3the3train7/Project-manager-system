b30490c99d4a166cdcb05142936a6e502f717b64
Ivan Mateshuk <7620536@mail.ru>
2022-10-11 11:59:16 +0300

    Added test: contextLoads, in module controller

---
+++ authservice/controller/src/test/java/com/example/authservice/AuthServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.example.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AuthServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
