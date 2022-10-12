fdc9fd5214c5e57887d957fa123635a79558e8e8
1thomas3the3train7 <mercyfirsov@gmail.com>
2022-10-12 22:20:03 +0400

    module userservice

---
+++ userservice/user_Repository/src/main/java/com/example/userservice/Model/Role/ShortRole.java
@@ -0,0 +1,22 @@
+package com.example.userservice.Model.Role;
+
+import com.example.userservice.Model.User.ShortUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "role")
+public class ShortRole extends BaseRole {
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_and_role",joinColumns = @JoinColumn(name = "role_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<ShortUser> users;
+}
